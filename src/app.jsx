ebc0d8cd7382ecb8f6309a15c7c5d1d9181263f1
Roy Oosterlee <roymcraft@gmail.com>
2018-01-24 11:46:05 +0100

    hoi
    hoi

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './components/main/master';
 import Admin from './components/admin/master';
+import * as f from './functions';
 
 class App extends React.Component {
 
     render() {
+        var q =  {
+            "keuzes" : [ {
+                "goed" : true,
+                "text" : "A"
+            }, {
+                "goed" : false,
+                "text" : "B"
+            }, {
+                "goed" : false,
+                "text" : "C"
+            }, {
+                "goed" : false,
+                "text" : "D"
+            } ],
+            "score" : 10,
+            "time" : 60,
+            "type" : "meerkeuze",
+            "vraag" : "De vraag?"
+        }
+
+
+        console.log(f.test());
+        // f.createQuiz("Naam", "Beschrijving");
+        f.updateQuizQuestions(0, q);
         return (
             <div className="app">
               <Router>
