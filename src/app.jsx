16b72466ffc43595ff36d5b161cda33e59570cd6
Roy Oosterlee <roymcraft@gmail.com>
2018-01-24 11:50:52 +0100

    uu
    uu

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,28 @@ import Main from './components/main/master';
 import Admin from './components/admin/master';
 import * as f from './functions';
 
+
+// var q =  {
+//     "keuzes" : [ {
+//         "goed" : true,
+//         "text" : "A"
+//     }, {
+//         "goed" : false,
+//         "text" : "B"
+//     }, {
+//         "goed" : false,
+//         "text" : "C"
+//     }, {
+//         "goed" : false,
+//         "text" : "D"
+//     } ],
+//     "score" : 10,
+//     "time" : 60,
+//     "type" : "meerkeuze",
+//     "vraag" : "De vraag?"
+// }
+
+
 class App extends React.Component {
 
     render() {
